3fcc59f81369dd3f24697e7c8c74e3c06a444d2b
boris-isac <borisisac@gmail.com>
2025-07-20 03:55:42 +0100

    [INIT] hospital app

---
+++ hospital/__manifest__.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Hospital Management System",
+    'license': 'LGPL-3',
+    'summary': "Odoo Tutorial",
+
+    'description': """
+Odoo tutorial app hospital
+    """,
+
+    'author': "BorisIsac",
+    'website': "###",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '17.0.1.0',
+
+    # any module necessary for this one to work correctly
+    'depends': [
+        'base',
+        'mail',
+    ],
+
+    # always loaded
+    'data': [
+        'security/ir.model.access.csv',
+        'views/menu.xml',
+        'views/patient_readonly_view.xml',
+        'views/patient_view.xml',
+
+
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
+
---
+++ hospital/models/patient.py
@@ -0,0 +1,26 @@
+from odoo import models, fields, api
+
+class HospitalPatient(models.Model):
+    _name = 'hospital.patient'
+    _inherit = ['mail.thread']
+    _description = 'Hospital patient data'
+
+
+
+    name = fields.Char(
+        string="Name",
+        required=True,
+        tracking=True
+    )
+
+    birthday = fields.Datetime(
+        string="Birthday",
+    )
+
+    gender = fields.Selection(
+        string="Gender",
+        selection=[
+            ('male', 'Male'),
+            ('female', 'Female'),
+        ]
+    )
