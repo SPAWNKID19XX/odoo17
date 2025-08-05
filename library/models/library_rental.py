2503543ebc35fc827905927ac5513a1dcc534d27
boris-isac <borisisac@gmail.com>
2025-08-05 19:09:11 +0100

    [ADD] smart button in library.books to print in pupop list of rentals

--- library/models/library_rental.py
+++ library/models/library_rental.py
@@ -1,12 +1,15 @@
 from odoo import models, fields, api
 
+
 class LibraryRental(models.Model):
     _name = 'library.rental'
     _description = 'Library Rental'
     _rec_name = 'book_id'
 
-    book_id = fields.Many2one('library.book', string='Book' )
-    customer_name = fields.Char()
-    rental_date = fields.Date()
+    book_id = fields.Many2one('library.book', string='Book', ondelete="cascade")
+    customer_name = fields.Char(required=True)
+    rental_date = fields.Date(required=True)
     # rental_price = fields.Float()
-    return_date = fields.Date()
+    return_date = fields.Date(required=True)
+
+
