7e95f2678a687ae50a82c90f47d0fae065ab73d4
boris-isac <borisisac@gmail.com>
2025-08-07 13:14:37 +0100

    [UPDATE] _sql_constraints for license_plate, @api.constrains for cars age and duration_trip

--- taxi/models/car_fleet.py
+++ taxi/models/car_fleet.py
@@ -1,23 +1,31 @@
 from datetime import datetime
-
-from dateutil.utils import today
+from odoo.exceptions import ValidationError
 from odoo import models, api, fields
 
 class CarFleet(models.Model):
     _name = 'car.fleet'
     _description = 'Car Fleet'
+    _rec_name = 'license_plate'
+
+    _sql_constraints = [
+        (
+            'license_plate',
+            'UNIQUE(license_plate)',
+            'Licence_plate is unique value'
+        )
+    ]
 
     name = fields.Char(string='Fleet Name', required=True)
     license_plate = fields.Char()
     made_year = fields.Integer()
-    driver_id = fields.Many2one('car.driver', string='Driver', required=True)
+    driver_id = fields.Many2one('car.driver', string='Driver')
     feature_ids = fields.Many2many(
         comodel_name='car.feature',
         string='Features',
     )
     car_age = fields.Integer(
         string='Car Age',
-        compute='car_age',
+        compute='_compute_car_age',
         store=True,
     )
 
@@ -30,6 +38,13 @@ class CarFleet(models.Model):
             else:
                 record.car_age = 0
 
+    @api.constrains('made_year')
+    def _validate_made_year(self):
+        current_year = datetime.today().year
+        for record in self:
+            if record.made_year and record.made_year > current_year:
+                raise ValidationError("Year Made cannot be in the future.")
 
 
-    #todo smart button show_trips
+
+    #TODO smart button show_trips
