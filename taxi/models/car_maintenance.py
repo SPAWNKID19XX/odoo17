b6e69d84f92b06737b326f73d0a94e43f2326990
boris-isac <borisisac@gmail.com>
2025-08-08 13:12:23 +0100

    [ADD] maintenance name on top

--- taxi/models/car_maintenance.py
+++ taxi/models/car_maintenance.py
@@ -1,16 +1,40 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 class CarMaintenance(models.Model):
     _name = 'car.maintenance'
     _description = 'Car Maintenance'
+    _rec_name = 'car_id_name'
 
     car_id = fields.Many2one(
         'car.fleet',
         string="Car id",
+        required=True,
     )
 
-    date = fields.Date()
+    date = fields.Date(
+        required=True,
+    )
 
     description = fields.Char(string="Description")
 
-    cost = fields.Float(string="Price")
+    cost = fields.Float(
+        required=True,
+        tring="Price"
+    )
+
+    car_id_name = fields.Char(
+        string="Car id name",
+        compute='_compute_car_id_name',
+        readonly=True,
+        store=True,
+    )
+
+    @api.depends('car_id')
+    def _compute_car_id_name(self):
+        for record in self:
+            if record.car_id:
+                record.car_id_name = record.car_id.name  # или license_plate, если нужно
+            else:
+                record.car_id_name = False
+
+
